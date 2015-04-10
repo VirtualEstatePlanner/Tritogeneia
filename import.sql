fa8bc67133222000538ba7fff76a00523e3a470d
George Georgulas <georgegeorgulas@gmail.com>
2015-04-10 11:40:32 -0400

    added missing .sql file to resolve issue with mobs link in FluxCP

--- import.sql
+++ import.sql
@@ -11,6 +11,7 @@ source /usr/bin/rathena/sql-files/item_cash_db.sql;
 source /usr/bin/rathena/sql-files/item_cash_db2.sql;
 source /usr/bin/rathena/sql-files/mob_db.sql;
 source /usr/bin/rathena/sql-files/mob_db2.sql;
+source /usr/bin/rathena/sql-files/mob_db2_re.sql;
 source /usr/bin/rathena/sql-files/mob_db_re.sql;
 source /usr/bin/rathena/sql-files/mob_skill_db.sql;
 source /usr/bin/rathena/sql-files/mob_skill_db2.sql;
